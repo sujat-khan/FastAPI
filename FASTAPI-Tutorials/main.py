1ffb04a457c97134d31d1f8a12a365cab1dc6af5
sujat-khan <sujatkhan24@gmail.com>
2026-06-07 12:29:33 -0400

    feat: initialize patient management API with data loader and basic endpoints

--- FASTAPI-Tutorials/main.py
+++ FASTAPI-Tutorials/main.py
@@ -1,12 +1,21 @@
 from fastapi import FastAPI
-
+import json
 app = FastAPI()
+
+def load_data():
+    with open ('patients.json','r') as f:
+        data =json.load(f)
+        return data
 
 @app.get("/")
 def hello():
-    return {'message':'Hello World'}
+    return {'message':'Patients Management System API'}
 
 
 @app.get("/about")
 def about():
-    return {'message':'I am learning about FastAPI'}
+    return {'message':'Fully functional API to manage your patient records'}
+
+@app.get("/view")
+def view():
+    return load_data()
