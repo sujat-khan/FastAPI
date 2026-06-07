e4c1804dea664d3f2c259abf21f73d08c53a5e7a
sujat-khan <sujatkhan24@gmail.com>
2026-06-07 13:06:31 -0400

    feat: Path and query parameters

--- FASTAPI-Tutorials/main.py
+++ FASTAPI-Tutorials/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, HTTPException, Query
 import json
 app = FastAPI()
 
@@ -19,3 +19,33 @@ def about():
 @app.get("/view")
 def view():
     return load_data()
+
+@app.get("/patient/{patient_id}")
+def view_patient(patient_id: str= Path(...,description='ID of the patient in Db', example='P001')):
+    #load all the patients
+    data =load_data()
+    #search for the patient ID
+    if patient_id in data:
+        return data[patient_id]
+    raise HTTPException(status_code = 404,detail="Patient Not Found")    
+
+@app.get("/sort")
+def sort_patient(sort_by:str=Query(..., description="Sort on the basis of height, weight or bmi", example='bmi'), order=Query('asc', description='sort in ascending or descending order')):
+    
+    Valid_fields=['height','weight','bmi']
+    if sort_by not in Valid_fields:
+        raise HTTPException(status_code=400, detail=f'Invalid field selected from {Valid_fields}')
+
+    if order not in ['asc','desc']:
+        raise HTTPException(status_code=400, detail='Invalid order selected. Select asc or desc')
+    
+    #load all the patients 
+    data = load_data()
+    # now we will sort the data
+    if order == 'desc':
+        reverse=True
+    else:
+        reverse=False
+    
+    sorted_data = dict(sorted(data.items(), key=lambda x: x[1][sort_by], reverse=reverse))
+    return sorted_data
