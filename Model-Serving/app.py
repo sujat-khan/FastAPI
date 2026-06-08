5c77f67f07a39475bbf3da108b8b4f95f4ca6f80
sujat-khan <sujatkhan24@gmail.com>
2026-06-07 21:21:55 -0400

    created Pydantic model for ML model inputs

---
+++ Model-Serving/app.py
@@ -0,0 +1,80 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, Field, computed_field
+from typing import Literal,Annotated
+import pickle
+import pandas as pd
+
+#import the ML model
+import os
+
+model_path = 'model.pkl' if os.path.exists('model.pkl') else 'Model-Serving/model.pkl'
+with open(model_path, 'rb') as f:
+    model = pickle.load(f)
+
+tier_1_cities = ["Mumbai", "Delhi", "Bangalore", "Chennai", "Kolkata", "Hyderabad", "Pune"]
+tier_2_cities = [
+    "Jaipur", "Chandigarh", "Indore", "Lucknow", "Patna", "Ranchi", "Visakhapatnam", "Coimbatore",
+    "Bhopal", "Nagpur", "Vadodara", "Surat", "Rajkot", "Jodhpur", "Raipur", "Amritsar", "Varanasi",
+    "Agra", "Dehradun", "Mysore", "Jabalpur", "Guwahati", "Thiruvananthapuram", "Ludhiana", "Nashik",
+    "Allahabad", "Udaipur", "Aurangabad", "Hubli", "Belgaum", "Salem", "Vijayawada", "Tiruchirappalli",
+    "Bhavnagar", "Gwalior", "Dhanbad", "Bareilly", "Aligarh", "Gaya", "Kozhikode", "Warangal",
+    "Kolhapur", "Bilaspur", "Jalandhar", "Noida", "Guntur", "Asansol", "Siliguri"
+]
+
+app =FastAPI()
+
+#making Pydantic model to validate data
+
+class UserInput(BaseModel):
+
+    age:Annotated[int, Field(..., gt=0, lt=120, description='age of the user')]
+    weight:Annotated[float, Field(..., gt=0, description='weight of the user')]
+    height:Annotated[float, Field(..., gt=0, lt=2.5, description='height of the user')]
+    income_lpa:Annotated[float, Field(..., gt=0, description='annual salary of the user')]
+    smoker:Annotated[bool, Field(...,  description='Is the user a smoker')]
+    city:Annotated[str, Field(..., description='city of the user')]
+    occupation: Annotated[Literal[ 'retired',     'freelancer',        'student', 'government_job',
+ 'business_owner',     'unemployed',    'private_job'], Field(..., description='occupation of the user')]
+
+    @computed_field
+    @property
+    def bmi(self) -> float:
+        return self.weight / (self.height ** 2)
+
+    @computed_field
+    @property
+    def lifestyle_risk(self)->str:
+        if self.smoker and self.bmi > 30:
+            return "high"
+        elif self.smoker or self.bmi > 27:
+            return "medium"
+        else:
+            return "low"
+
+    @computed_field
+    @property
+    def age_group(self)->str:
+        if self.age < 25:
+            return "young"
+        elif self.age < 45:
+            return "adult"
+        elif self.age < 60:
+            return "middle_aged"
+        return "senior"
+
+    @computed_field
+    @property
+    def city_tier(self)->int:
+        if self.city in tier_1_cities:
+            return 1
+        elif self.city in tier_2_cities:
+            return 2
+        else:
+            return 3
+
+
+
+
+
+ 
+
